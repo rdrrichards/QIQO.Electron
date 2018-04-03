b710f4d0fdc75bbb0798367973d0fcac841ca09b
R. Richards <rdrrichards@hotmail.com>
2018-04-02 19:18:30 -0500

    added some routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AccessComponent } from './access/access.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'access', pathMatch: 'full' },
+      { path: 'access', component: AccessComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
