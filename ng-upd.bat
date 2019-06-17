543ea168aa97ce8f9bc6dba2ac679d1fe8c79843
R. Richards <rdrrichards@hotmail.com>
2019-06-17 15:54:39 -0500

    batch

--- ng-upd.bat
+++ ng-upd.bat
@@ -1,6 +1,10 @@
+CALL git commit -am "pre update commit"
 CALL ng update @angular/cli
-CALL git commit -m "ng cli updates to latest"
+CALL git commit -am "cli update to latest"
 CALL ng update @angular/core
+CALL git commit -am "ng update to latest"
+CALL npm audit fix
+CALL git commit -am "post update commit"
 
 CALL npm update electron
 CALL npm run electron
